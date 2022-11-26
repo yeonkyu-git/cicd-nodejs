29ada2e68e4b158275877ecaa6298b63942b650f
yeonkyu <yk0924.kim@gmail.com>
2022-11-26 10:10:31 +0900

    cicd-test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world! This is node APP for CICD");
 })
 
 app.listen(port, () => {
